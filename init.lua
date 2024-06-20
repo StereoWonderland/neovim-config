f478ab185e5d003eb8dcb85d272e75bfbf4dfadb
Mainthan Suriyakumaran <mainthan2@gmail.com>
2024-06-20 01:40:46 +0100

    add treesitter support for dockerfile and terraform

--- init.lua
+++ init.lua
@@ -81,7 +81,7 @@ require("lazy").setup({
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "python" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "dockerfile", "terraform" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
